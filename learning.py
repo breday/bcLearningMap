d16428a421e889f4aa4956f94e812bd75cdd5af7
Jjelimo <jolynejelimo@gmail.com>
2017-03-10 15:10:07 +0300

    Our final project on Learning Map

--- learning.py
+++ learning.py
@@ -4,19 +4,28 @@ from learning_cli import LearningMap
 
 
 def main():
-    print("\n\n")
-    print("========================**********Welcome to my learning Map\n\n******=====================")
-    count = 0 
-    print(" 1. Add skills\n 2. View all skills\n 3. Add studied Skill \n 4. View Studied Skill\n 5. View skill not studied \n 6. View my learning map\n 6. Exit\n \n")
-     action = input('Enter your choice: ')
-     if action == "1":
-     elif action == "2":
-     elif action == "3":
-     elif action == "4":
-     elif action == "5":
-     elif action == "6":
-        break
-
-     else:
-        print("Invalid choice")
+    while True:
+        print("\n\n")
+        print("******Welcome to my learning Map\n\n******")
+        print(" 1. Add skills\n 2. View all skills\n 3. Add studied Skill \n 4. View Studied Skill\n 5. View skill not studied \n 6. View my learning map\n 7. Exit\n \n")
+        action = input('Enter your choice: ')
+        learning = LearningMap()
+        if action == "1":
+            learning.add_skills()
+        elif action == "2":
+            learning.view_skills()
+        elif action == "3":
+            learning.add_skill_studied()
+        elif action == "4":
+            learning.view_skills_studied()
+        elif action == "5":
+            learning.view_skills_not_studied()
+        elif action == "6":
+            learning.view_learning_map()
+        elif action == "7":
+            break
+        else:
+            print("Invalid choice")
         
+
+if __name__ == '__main__': main()
--- learning_cli.py
+++ learning_cli.py
@@ -1,24 +1,26 @@
+all_skills = []
+studied_skills = []
 class LearningMap(object):
-	all_skills = []
-	studied_skills = []
+    def __init__(self):
+        pass
+    
+    def add_skills(self):
+        print ("======= Enter the number of skills======")
+    
+        skills = input("How many skills do you want to add? ")
+        for  skills in range(int(skills)):
+            self.skill_name = input("Add skill: ")
+            all_skills.append(self.skill_name)
 
-	def __init__(self):
-		pass
-		
-
-    def add_skills(self, skill_name):
-    	print ("======= Enter the number of skills======")
-    	skills = input("How many skills do want to add?")
-    	for  skills in range(int(skills)):
-    		self.skill_name = input("Add skill")
-    		all_skills.append(self.skill_name)
     def  view_skills(self):
     	for skill in all_skills:
-    		print "The list of all skills added are: %s" %(skill)
-    def add_skill_studied(self, skill_name):
-    	print "====== Kindly add the skills you have studied ========="
-    	self.skill_name = input("Skill name")
-    	studied_skills.append(self.skill_name)
+    		print (skill)
+    def add_skill_studied(self):
+        print ("====== Kindly add the skills you have studied =========")
+        skills = input("How many skills have you studied so far? ")
+        for  skills in range(int(skills)):
+            self.skill_name = input("Skill name: ")
+            studied_skills.append(self.skill_name)
     	
     def view_skills_studied(self):
     	for skill in studied_skills:
@@ -33,13 +35,13 @@ class LearningMap(object):
     	
 
     def view_learning_map(self):
-    	print ("====================" +View my learningmap +"====================")
+    	print ("====================" +"View my learningmap "+"====================")
 
-    	print ("====================" +This iss what I planned to study +"====================")
+    	print ("====================" +"This is what I planned to study "+"====================")
     	for skills in all_skills:
     		print(skills)
-    	print ("====================" +This what I have studied +"====================")
+    	print ("====================" +"This what I have studied" +"====================")
     	for skills in studied_skills:
     		print(skills)
-    	print ("====================" +This what I have notyet studied + "====================")
-    	view_skills_not_studied()	
+    	print ("====================" +"This what I have not yet studied" + "====================")
+    	self.view_skills_not_studied()	
