9bb3cce81354eb18653788014bfc29b7b7f39f94
angieMutava <mutavaangela@yahoo.com>
2017-03-11 00:20:55 -0800

    added the files

---
+++ learning_cli.py
@@ -0,0 +1,22 @@
+class LearningMap(object):
+	all_skills = []
+	studied_skills = []
+
+	def __init__(self):
+		pass
+		
+
+    def add_skills():
+    	pass
+    def  view_skills():
+    	pass
+    def add_skill_studied():
+    	pass
+    def view_skills_studied():
+    	pass
+
+    def view_skills_not_studied():
+    	pass
+
+    def view_learning_map():
+    	pass		 					
